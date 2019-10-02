833e79fa3966c196cfde21873d15a5ff5dcc6c0b
Charles Bezerra <charlesbezerra5@gmail.com>
2019-10-02 18:48:57 -0300

    ClienteControllerTest alterado

--- SAGA/testes/lab5/ClienteControllerTest.java
+++ SAGA/testes/lab5/ClienteControllerTest.java
@@ -6,49 +6,123 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ClienteControllerTest {
 
+    private ClienteController clientes;
+
+    @BeforeEach
+    void criar objetos() {
+         this.clientes = new ClienteController();
+         this.clientes.adiciona("017.626.944-41", "Charles", "charlesbezerra5@gmail.com", "SPLab")
+    }
+
     @Test
     void testAdicionaComCpfNulo() {
+        try {
+            this.clientes.adiciona(null, "Cliente 1", "cliente@gmail.com","LabCliente");
+            fail("Exceção deveria ser lançada!");
+        }catch (IllegalArgumentException error){
+            assertEquals("java.lang.IllegalArgumentException: Erro no cadastro do cliente: cpf nao pode ser vazio ou nulo.", error.toString());
+        }      
     }
 
     @Test
     void testAdicionaComCpfInvalido() {
+        try {
+            this.clientes.adiciona("", "Cliente 1", "cliente@gmail.com","LabCliente");
+            fail("Exceção deveria ser lançada!");
+        }catch (IllegalArgumentException error){
+            assertEquals("java.lang.IllegalArgumentException: Erro no cadastro do cliente: cpf nao pode ser vazio ou nulo.", error.toString());
+        }
     }
 
     @Test
     void testAdicionaComNomeNulo() {
+        try {
+            this.clientes.adiciona("12345", null, "cliente@gmail.com","LabCliente");
+            fail("Exceção deveria ser lançada!");
+        }catch (IllegalArgumentException error){
+            assertEquals("java.lang.IllegalArgumentException: Erro no cadastro do cliente: nome nao pode ser vazio ou nulo.", error.toString());
+        }
     }
 
     @Test
     void testAdicionaComNomeInvalido() {
+        try {
+            this.clientes.adiciona("12345", "", "cliente@gmail.com","LabCliente");
+            fail("Exceção deveria ser lançada!");
+        }catch (IllegalArgumentException error){
+            assertEquals("java.lang.IllegalArgumentException: Erro no cadastro do cliente: nome nao pode ser vazio ou nulo.", error.toString());
+        }
     }
 
     @Test
     void testAdicionaComEmailNulo() {
+        try {
+            this.clientes.adiciona("12345", "Cliente 1", null,"LabCliente");
+            fail("Exceção deveria ser lançada!");
+        }catch (IllegalArgumentException error){
+            assertEquals("java.lang.IllegalArgumentException: Erro no cadastro do cliente: email nao pode ser vazio ou nulo.", error.toString());
+        }
     }
 
     @Test
     void testAdicionaComEmailInvalido() {
+        try {
+            this.clientes.adiciona("12345", "Cliente 1", "","LabCliente");
+            fail("Exceção deveria ser lançada!");
+        }catch (IllegalArgumentException error){
+            assertEquals("java.lang.IllegalArgumentException: Erro no cadastro do cliente: email nao pode ser vazio ou nulo.", error.toString());
+        }
     }
 
     @Test
     void testAdicionaComLocalizacaoNula() {
+        try {
+            this.clientes.adiciona("12345", "Cliente 1", "cliente@gmail.com", null);
+            fail("Exceção deveria ser lançada!");
+        }catch (IllegalArgumentException error){
+            assertEquals("java.lang.IllegalArgumentException: Erro no cadastro do cliente: localizacao nao pode ser vazio ou nulo.", error.toString());
+        }
     }
 
     @Test
     void testAdicionaComLocalizacaoInvalida() {
+        try {
+            this.clientes.adiciona("12345", "Cliente 1", "cliente@gmail.com","");
+            fail("Exceção deveria ser lançada!");
+        }catch (IllegalArgumentException error){
+            assertEquals("java.lang.IllegalArgumentException: Erro no cadastro do cliente: localizacao nao pode ser vazio ou nulo.", error.toString());
+        }
     }
 
 
     @Test
+    void testExibeComClienteInegistente(){
+         try {
+            this.clientes.exibe("12345");
+            fail("Exceção deveria ser lançada!");
+        }catch (IllegalArgumentException error){
+            assertEquals("java.lang.IllegalArgumentException: Erro na exibicao do cliente: cliente não existe", error.toString());
+        }
+    }
+
+    @Test
     void testExibeComCpfNulo() {
+        try {
+            this.clientes.exibe(null);
+            fail("Exceção deveria ser lançada!");
+        }catch (IllegalArgumentException error){
+            assertEquals("java.lang.IllegalArgumentException: Erro na exibicao do cliente: cpf nao pode ser vazio ou nulo.", error.toString());
+        }
     }
 
     @Test
     void testExibeComCpfInvalido() {
-    }
-
-    @Test
-    void testExibeComClienteInexistente() {
+        try {
+            this.clientes.exibe("");
+            fail("Exceção deveria ser lançada!");
+        }catch (IllegalArgumentException error){
+            assertEquals("java.lang.IllegalArgumentException: Erro na exibicao do cliente: cpf nao pode ser vazio ou nulo.", error.toString());
+        }
     }
 
     @Test
