d22ff09d8b5fc9107deacad85ac6acc574a674d4
119110595 CHARLES BEZERRA DE OLIVEIRA JUNIOR <charlesboj@lcc3-077.lcc.ufcg.edu.br>
2019-10-10 11:02:00 -0300

    Teste adicionado

---
+++ SAGA/testes/lab5/ProdutoControllerTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package lab5;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author charlesboj
+ *
+ */
+class ProdutoControllerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
